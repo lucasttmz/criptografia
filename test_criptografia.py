8a0891b855e78bf6c051e93ebfc1745b91fb8b67
lucasttmz <lucasttmz@gmail.com>
2022-09-28 16:35:49 -0300

    formatado

--- test_criptografia.py
+++ test_criptografia.py
@@ -17,56 +17,87 @@ def testes():
     Teste para varios casos especificos, todos com sucesso.
     """
     #Normal
-    criptografar_teste(chave="Chave inserida aqui para criar o fluxo", 
-                       mensagem="Mensagem a ser criptografada")
+    criptografar_teste(
+        chave="Chave inserida aqui para criar o fluxo", 
+        mensagem="Mensagem a ser criptografada")
 
     #Numeros
-    criptografar_teste(chave="123454321013245684844512", 
-                       mensagem="543212345432115616558")
+    criptografar_teste(
+        chave="123454321013245684844512", 
+        mensagem="543212345432115616558")
 
     #Char Especial
-    criptografar_teste(chave="#+@ $_% ^-&=D ]-[()", 
-                       mensagem="@#@!@#$12_= 123*48abc84 @&#@#")
+    criptografar_teste(
+        chave="#+@ $_% ^-&=D ]-[()", 
+        mensagem="@#@!@#$12_= 123*48abc84 @&#@#")
 
     #Char Escape
-    criptografar_teste(chave="\t123\t321\n__\-", 
-                       mensagem="\r123\n242GG82\t11")
+    criptografar_teste(
+        chave="\t123\t321\n__\-", 
+        mensagem="\r123\n242GG82\t11")
 
     #Mensagem Longa
-    criptografar_teste(chave="abcdefghijklkmsmhent",
-                       mensagem="aaaaaaaaaaa aaaaaaaaaaaaaaaaa aaaaaaaaaabcde" +
-                       "aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaa aaaaaabcde" +
-                       "aaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaaaabcde" +
-                       "aaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaa aaaaaabcde" +
-                       "aaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa aaaaaaaaabcde")
+    criptografar_teste(
+        chave="abcdefghijklkmsmhent",
+        mensagem="aaaaaaaaaaa aaaaaaaaaaaaaaaaa aaaaaaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaa aaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaaaabcde" +
+        "aaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaa aaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaa aaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaaaabcde" +
+        "aaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaa aaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaa aaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaaaabcde" +
+        "aaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaa aaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa aaaaaaaaabcde")
 
     #Chave Longa
-    criptografar_teste(chave="aaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaa aaaaaabcde" +
-                       "aaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaabcde" +
-                       "aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaa aaaaaaabcde" +
-                       "aaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaa aaaaaaaaaaabcde" +
-                       "aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaa aaaaaabcde", 
-                       mensagem="abcdefghijklmnopqrstuvwyxz123456790")
+    criptografar_teste(
+        chave="aaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaa aaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaa aaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaaaabcde" +
+        "aaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaa aaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaa aaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaaaabcde" +
+        "aaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaa aaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaa aaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaaaabcde" +
+        "aaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaa aaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaa aaaaaabcde", 
+        mensagem="abcdefghijklmnopqrstuvwyxz123456790")
 
     #Ambos Longos
-    criptografar_teste(chave="@@lskiia__ sajidij lalislooo sjiajisjjssisabcde" +
-                       "@@lskiia__ sajidij lallaoiislooo sjiajisjaijssisabcde" +
-                       "@@lskiia__ sajidij lallaoiislooo sjiajisjaijssisabcde" +
-                       "@@lskiia__ sajidij lallaoiislooo sjiajisjjisjaijssise" +
-                       "@@lskiia__ sajidij lallaoiislooo sjiajjaijssisabcde", 
-                       mensagem="aaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaabcde" +
-                       "aaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcde" +
-                       "aaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaabcde" +
-                       "aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaabcde" +
-                       "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaabcde")
+    criptografar_teste(
+        chave="@@lskiia__ sajidij lalislooo sjiajisjjssisabcde" +
+        "@@lskiia__ sajidij lallaoiislooo sjiajisjaijssisabcde" +
+        "@@lskiia__ sajidij lallaoiislooo sjiajisjaijssisabcde" +
+        "@@lskiia__ sajidij lallaoiislooo sjiajisjjisjaijssise" +
+        "@@lskiia__ sajidij lallaoiislooo sjiajisjaijssisabcde" +
+        "@@lskiia__ sajidij lallaoiislooo sjiajisjjisjaijssise" +
+        "@@lskiia__ sajidij lallaoiislooo sjiajisjaijssisabcde" +
+        "@@lskiia__ sajidij lallaoiislooo sjiajisjaijssisabcde" +
+        "@@lskiia__ sajidij lallaoiislooo sjiajisjjisjaijssise" +
+        "@@lskiia__ sajidij lallaoiislooo sjiajjaijssisabcde", 
+        mensagem="aaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaabcde" +
+        "aaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaabcde" +
+        "aaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaabcde" +
+        "aaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaabcde" +
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaabcde")
     
     #Chave com apenas um espaço em branco
-    criptografar_teste(chave=" ", 
-                       mensagem="A chave esta com um espaco em branco")
+    criptografar_teste(
+        chave=" ", 
+        mensagem="A chave esta com um espaco em branco")
 
     #Mensagem com apenas um espaço em branco
-    criptografar_teste(chave="A mensagem esta com um espaco em branco",
-                       mensagem=" ")
+    criptografar_teste(
+        chave="A mensagem esta com um espaco em branco",
+        mensagem=" ")
 
     #Se um dos testes não der True essa mensagem não aparece
     print("Se essa mensagem apareceu, tudo funcionou como deveria")
